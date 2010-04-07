f94e382fb417ec7fbd2c284510d17795cd708222
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-04-06 21:56:36 -0600

    openerp-install: added addons-zip.sh

---
+++ install-scripts/openerp-install/addons-zip.sh
@@ -0,0 +1,39 @@
+#       addons-zip.sh
+#       
+#       Copyright 2010 ClearCorp S.A. <info@clearcorp.co.cr>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#!/bin/bash
+
+if [[ ! -d $LIBBASH_CCORP_DIR ]]; then
+	echo "libbash-ccorp not installed."
+	exit 1
+fi
+
+#~ Libraries import
+. $LIBBASH_CCORP_DIR/main-lib/checkRoot.sh
+. $LIBBASH_CCORP_DIR/main-lib/getDist.sh
+
+# Check user is root
+checkRoot
+
+
+list=$(find $2 -iname "__terp__.py" | awk '{system("dirname "$1)}')
+for i in $list; do
+	echo $i
+	zip -r $i.zip $i
+	rm -r $i
+done
