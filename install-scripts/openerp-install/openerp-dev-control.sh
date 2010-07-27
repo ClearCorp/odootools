82debdf1ca7ab4ce22d34001b3c0deb3d5de2ed0
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-07-26 20:24:21 -0600

    chown openerp-dev-control.sh

--- install-scripts/openerp-install/openerp-dev-control.sh
+++ install-scripts/openerp-install/openerp-dev-control.sh
@@ -8,68 +8,68 @@ case $2 in
 server)
 	case $3 in
 	start)
-		proc_ctl openerp-server-$1 start
+		gksudo service openerp-server-$1 start
 		;;
 	stop)
-		proc_ctl openerp-server-$1 stop
+		gksudo service openerp-server-$1 stop
 		;;
 	restart)
-		proc_ctl openerp-server-$1 restart
+		gksudo service openerp-server-$1 restart
 		;;
 	esac
 	;;
 web)
 	case $3 in
 	start)
-		proc_ctl openerp-web-$1 start
+		gksudo service openerp-web-$1 start
 		;;
 	stop)
-		proc_ctl openerp-web-$1 stop
+		gksudo service openerp-web-$1 stop
 		;;
 	restart)
-		proc_ctl openerp-web-$1 restart
+		gksudo service openerp-web-$1 restart
 		;;
 	esac
 	;;
 all)
 	case $3 in
 	start)
-		proc_ctl openerp-server-$1 start
-		proc_ctl openerp-web-$1 start
+		gksudo service openerp-server-$1 start
+		gksudo service openerp-web-$1 start
 		;;
 	stop)
-		proc_ctl openerp-server-$1 stop
-		proc_ctl openerp-web-$1 stop
+		gksudo service openerp-server-$1 stop
+		gksudo service openerp-web-$1 stop
 		;;
 	restart)
-		proc_ctl openerp-server-$1 restart
-		proc_ctl openerp-web-$1 restart
+		gksudo service openerp-server-$1 restart
+		gksudo service openerp-web-$1 restart
 		;;
 	esac
 	;;
 apache)
 	case $2 in
 	start)
-		proc_ctl apache2 start
+		gksudo service apache2 start
 		;;
 	stop)
-		proc_ctl apache2 stop
+		gksudo service apache2 stop
 		;;
 	restart)
-		proc_ctl apache2 restart
+		gksudo service apache2 restart
 		;;
 	esac
 	;;
 postgresql)
 	case $2 in
 	start)
-		proc_ctl postgresql-8.4 start
+		gksudo service postgresql-8.4 start
 		;;
 	stop)
-		proc_ctl postgresql-8.4 stop
+		gksudo service postgresql-8.4 stop
 		;;
 	restart)
-		proc_ctl postgresql-8.4 restart
+		gksudo service postgresql-8.4 restart
 		;;
 	esac
 	;;
