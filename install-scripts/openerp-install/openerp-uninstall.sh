aab20bfe956ec18376e0bc58ee193b41a0df565d
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-08-21 14:02:16 -0600

    [IMP] openerp-uninstall.sh: uninstall script

---
+++ install-scripts/openerp-install/openerp-uninstall.sh
@@ -0,0 +1,81 @@
+#       openerp-uninstall.sh
+#       
+#       Copyright 2010 ClearCorp S.A. <info@clearcorp.co.cr>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#!/bin/bash
+
+if [[ ! -d $LIBBASH_CCORP_DIR ]]; then
+	echo "libbash-ccorp not installed."
+	exit 1
+fi
+
+#~ Libraries import
+. $LIBBASH_CCORP_DIR/main-lib/checkRoot.sh
+. $LIBBASH_CCORP_DIR/main-lib/getDist.sh
+
+# Check user is root
+checkRoot
+
+# Print title
+echo "OpenERP uninstallation script"
+echo "---------------------------"
+echo ""
+
+# Set distribution
+dist=""
+getDist dist
+echo "Distribution: $dist"
+echo ""
+
+# Sets vars corresponding to the distro
+if [[ $dist == "lucid" ]]; then
+	# Ubuntu 10.04, python 2.6
+	posgresql_rel=8.4
+	python_rel=python2.6
+	ubuntu_rel=10.04
+	base_path=/usr/local
+	install_path=$base_path/lib/$python_rel/dist-packages
+	addons_path=$install_path/addons/
+	sources_path=$base_path/src/openerp
+else
+	# Only Lucid supported for now
+	echo "ERROR: This program must be executed on Ubuntu Lucid 10.04 (Desktop or Server)"
+	exit 1
+fi
+
+echo "rm -r /etc/openerp"
+rm -r /etc/openerp
+echo "rm -r /var/log/openerp"
+rm -r /var/log/openerp
+echo "rm -r /var/run/openerp"
+rm -r /var/run/openerp
+echo "rm -r $install_path/openerp-*"
+rm -r $install_path/openerp-*
+echo "rm -r $base_path/bin/openerp-*"
+rm -r $base_path/bin/openerp-*
+for i in $(ls /etc/init.d/openerp*); do
+	echo "updaterc.d disable $i"
+	updaterc.d disable $i
+	echo "rm /etc/init.d/$i"
+	rm /etc/init.d/$i
+done
+echo "rm -r $base_path/share/man/man1/openerp*"
+rm -r $base_path/share/man/man1/openerp*
+echo "rm -r $base_path/share/man/man5/openerp*"
+rm -r $base_path/share/man/man5/openerp*
+
+exit 0
