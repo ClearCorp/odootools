524b948d240644ef07ee24d801104f19c2526a68
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-12-17 18:57:15 -0600

    [IMP] Maverick support on koo-install.sh

--- install-scripts/openerp-dev-station/koo-install.sh
+++ install-scripts/openerp-dev-station/koo-install.sh
@@ -68,6 +68,14 @@ if [[ $dist == "lucid" ]]; then
 	base_path=/usr/local
 	install_path=$base_path/lib/$python_rel/dist-packages
 	sources_path=$base_path/src
+elif [[ $dist == "maverick" ]]; then
+	# Ubuntu 10.10, python 2.6
+	posgresql_rel=8.4
+	python_rel=python2.6
+	ubuntu_rel=10.10
+	base_path=/usr/local
+	install_path=$base_path/lib/$python_rel/dist-packages
+	sources_path=$base_path/src
 else
 	# Only Karmic supported for now
 	log_echo "ERROR: This program must be executed on Ubuntu Lucid 10.04 (Desktop or Server)"
