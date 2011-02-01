8ebb3eb22cd496b55257971b274b0af50c1a5d78
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2011-02-01 16:49:52 -0600

    [ADD] apache-erp-6.0

--- install-scripts/openerp-install/openerp-lib.sh
+++ install-scripts/openerp-install/openerp-lib.sh
@@ -425,7 +425,7 @@ function install_apache {
 	fi
 
 	log_echo "Configuring site config files..."
-	cp $LIBBASH_CCORP_DIR/install-scripts/openerp-install/apache-erp /etc/apache2/sites-available/erp >> $INSTALL_LOG_FILE
+	cp $LIBBASH_CCORP_DIR/install-scripts/openerp-install/apache-erp-$branch /etc/apache2/sites-available/erp >> $INSTALL_LOG_FILE
 	mkdir -p /etc/openerp/apache2/rewrites >> $INSTALL_LOG_FILE
 	cp $LIBBASH_CCORP_DIR/install-scripts/openerp-install/apache-ssl-skeleton /etc/openerp/apache2/ssl-skeleton >> $INSTALL_LOG_FILE
 	sed -i "s/ServerAdmin webmaster@localhost/ServerAdmin support@clearnet.co.cr\n\n\tInclude \/etc\/apache2\/sites-available\/erp/g" /etc/apache2/sites-available/default >> $INSTALL_LOG_FILE
