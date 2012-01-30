ee5965d41bda9ca3c0c1676997025976183b3dfe
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2012-01-29 18:28:55 -0600

    [FIX] openerp-lib.sh: comments

--- openerp-scripts/openerp-lib.sh
+++ openerp-scripts/openerp-lib.sh
@@ -121,10 +121,10 @@ function install_python_lib {
 	packages+=(graphviz)
 	packages+=(ghostscript)
 	packages+=(python-imaging)
-	packages+=(python-libxslt1)	#Excel spreadsheets
+	packages+=(python-libxslt1) #Excel spreadsheets
 	packages+=(python-matplotlib)
-	packages+=(python-openssl)	#Extra for ssl ports
-	packages+=(python-xlwt)		#Excel spreadsheets
+	packages+=(python-openssl)  #Extra for ssl ports
+	packages+=(python-xlwt)     #Excel spreadsheets
 
 	# Packages for 5.0 server
 	if [[ $branch ~= "5.0" ]]; then
@@ -145,7 +145,7 @@ function install_python_lib {
 		packages+=(python-yaml)
 
 		# Recomended
-		packages+=(python-webdav)		#For document-webdav
+		packages+=(python-webdav)       #For document-webdav
 	fi
 
 	# Packages for 6.1 server
@@ -155,7 +155,7 @@ function install_python_lib {
 		packages+=(python-zsi)
 	
 		# Recomended
-		packages+=(python-gdata)		#Google data parser
+		packages+=(python-gdata)        #Google data parser
 		packages+=(python-ldap)
 		packages+=(python-openid)
 		packages+=(python-vatnumber)
@@ -189,8 +189,8 @@ function install_python_lib {
 		packages+=(python-werkzeug)
 	
 		# Recomended
-		packages+=(python-mock)		# For testing
-		packages+=(python-unittest2)	# For testing
+		packages+=(python-mock)         # For testing
+		packages+=(python-unittest2)    # For testing
 	fi
 	
 	# Install packages
