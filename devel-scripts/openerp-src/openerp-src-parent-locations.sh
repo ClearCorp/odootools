e2a9285d90c4714c07e0f0757ec34c0d4b341c6c
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2012-06-11 08:37:02 -0600

    [IMP] fixed parent locations

--- devel-scripts/openerp-src/openerp-src-parent-locations.sh
+++ devel-scripts/openerp-src/openerp-src-parent-locations.sh
@@ -8,31 +8,31 @@ OPENERP_REPO_BASE=$(readlink -m $OPENERP_REPO_BASE)
 
 REPO_DIR=$OPENERP_REPO_BASE/openerp-src/src
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-server/5.0-ccorp" > $REPO_DIR/5.0/openobject-server/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-server/6.0-ccorp" > $REPO_DIR/6.0/openobject-server/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-server/6.1-ccorp" > $REPO_DIR/6.1/openobject-server/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-server/trunk-ccorp" > $REPO_DIR/trunk/openobject-server/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-server/5.0-ccorp" > $REPO_DIR/5.0/openobject-server/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-server/6.0-ccorp" > $REPO_DIR/6.0/openobject-server/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-server/6.1-ccorp" > $REPO_DIR/6.1/openobject-server/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-server/trunk-ccorp" > $REPO_DIR/trunk/openobject-server/.bzr/branch/branch.conf
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/5.0-ccorp" > $REPO_DIR/5.0/openobject-addons/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/6.0-ccorp" > $REPO_DIR/6.0/openobject-addons/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/6.1-ccorp" > $REPO_DIR/6.1/openobject-addons/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/trunk-ccorp" > $REPO_DIR/trunk/openobject-addons/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/5.0-ccorp" > $REPO_DIR/5.0/openobject-addons/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/6.0-ccorp" > $REPO_DIR/6.0/openobject-addons/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/6.1-ccorp" > $REPO_DIR/6.1/openobject-addons/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/trunk-ccorp" > $REPO_DIR/trunk/openobject-addons/.bzr/branch/branch.conf
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/extra-5.0-ccorp" > $REPO_DIR/5.0/openobject-addons-extra/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/extra-6.0-ccorp" > $REPO_DIR/6.0/openobject-addons-extra/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/extra-trunk-ccorp" > $REPO_DIR/trunk/openobject-addons-extra/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/extra-5.0-ccorp" > $REPO_DIR/5.0/openobject-addons-extra/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/extra-6.0-ccorp" > $REPO_DIR/6.0/openobject-addons-extra/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/extra-trunk-ccorp" > $REPO_DIR/trunk/openobject-addons-extra/.bzr/branch/branch.conf
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client/5.0-ccorp" > $REPO_DIR/5.0/openobject-client/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client/6.0-ccorp" > $REPO_DIR/6.0/openobject-client/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client/6.1-ccorp" > $REPO_DIR/6.1/openobject-client/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client/trunk-ccorp" > $REPO_DIR/trunk/openobject-client/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client/5.0-ccorp" > $REPO_DIR/5.0/openobject-client/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client/6.0-ccorp" > $REPO_DIR/6.0/openobject-client/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client/6.1-ccorp" > $REPO_DIR/6.1/openobject-client/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client/trunk-ccorp" > $REPO_DIR/trunk/openobject-client/.bzr/branch/branch.conf
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client-web/5.0-ccorp" > $REPO_DIR/5.0/openobject-client-web/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-client-web/6.0-ccorp" > $REPO_DIR/6.0/openobject-client-web/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openerp-web/6.1-ccorp" > $REPO_DIR/6.1/openerp-web/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openerp-web/trunk-ccorp" > $REPO_DIR/trunk/openerp-web/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client-web/5.0-ccorp" > $REPO_DIR/5.0/openobject-client-web/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-client-web/6.0-ccorp" > $REPO_DIR/6.0/openobject-client-web/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openerp-web/6.1-ccorp" > $REPO_DIR/6.1/openerp-web/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openerp-web/trunk-ccorp" > $REPO_DIR/trunk/openerp-web/.bzr/branch/branch.conf
 
 
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-server/trunk-ccorp" > $REPO_DIR/openerp/trunk/openobject-server/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openerp-web/trunk-ccorp" > $REPO_DIR/openerp/trunk/openerp-web/.bzr/branch/branch.conf
-echo "parent_location = http://bazaar.launchpad.net/~clearcorp/openobject-addons/trunk-ccorp" > $REPO_DIR/openerp/trunk/openobject-addons/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-server/trunk-ccorp" > $REPO_DIR/openerp/trunk/openobject-server/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openerp-web/trunk-ccorp" > $REPO_DIR/openerp/trunk/openerp-web/.bzr/branch/branch.conf
+echo "parent_location = http://bazaar.launchpad.net/~clearcorp-drivers/openobject-addons/trunk-ccorp" > $REPO_DIR/openerp/trunk/openobject-addons/.bzr/branch/branch.conf
