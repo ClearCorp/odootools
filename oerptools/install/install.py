626144d6719af4c16df491e7d1e52eeb43c56a81
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2013-01-23 21:25:07 -0600

    [IMP] more work on installation

--- oerptools/install/install.py
+++ oerptools/install/install.py
@@ -29,19 +29,13 @@ WARNING:    If you update this file, please remake the installer and
             if the oerptools are installed.
 '''
 
-import logging
+import os, shutil, logging
 from oerptools.lib import config, tools, bzr
 
 _logger = logging.getLogger('oerptools.install.install')
 
 
 def install():
-    _logger.debug('Checking if user is root')
-    tools.exit_if_not_root('oerptools-install')
-    
-    _logger.debug('Installing bzr')
-    bzr.bzr_install()
-    
     
     def oerptools_install(install_dir):
         os_version = tools.get_os()
@@ -56,16 +50,50 @@ def install():
     def ubuntu_oerptools_install(install_dir):
         import re
         
+        _logger.info('Installing oerptools')
+        
         # Initialize bzr
         bzr.bzr_initialize()
+        from bzrlib.branch import Branch
         
-        _logger.info('Installing oerptools')
+        # Get this oerptools branch directory
+        branch_path = os.path.abspath(os.path.dirname(__file__)+"/..")
+        
+        # Values dict to store default config values for this install
+        values = {
+            'main': {},
+            'logging': {},
+            }
+        
+        # Set oerptools_path for new file
+        values['main'].update({'oerptools_path': install_dir})
+        
+        # Set config_file for new file
+        if 'install_config_path' in config.params:
+            values['main'].update({'config_file': config.params['install_config_path']})
+        elif 'config_file' in config.params and config.params['config_file']:
+            values['main'].update({'config_file': config.params['config_file'][-1]})
+        else:
+            values['main'].update({'config_file': '/etc/oerptools/settings.conf'})
+            
+        # Set log_file for new file
+        if 'log_file' in config.params:
+            values['logging'].update({'log_file': config.params['log_file']})
+        else:
+            values['logging'].update({'log_file': '/var/log/oerptools/oerptools.log'})
+            
+        # Set log_handler for new file
+        if 'log_handler' in config.params:
+            values['logging'].update({'log_handler': ','.join(config.params['log_handler'])})
+        else:
+            values['logging'].update({'log_handler': ':INFO'})
+        
         
         f = open('/etc/environment')
         environment = f.read()
         f.close()
         if 'OERPTOOLS_DIR' in environment:
-            environment = re.sub(r'OERPTOOLS_DIR.*','OERPTOOLS_DIR=%s' % (install_dir, environment))
+            environment = re.sub(r'OERPTOOLS_DIR.*','OERPTOOLS_DIR=%s' % install_dir, environment)
             f = open('/etc/environment','w+')
             f.write(environment)
         else:
@@ -74,47 +102,69 @@ def install():
         f.close()
         
         if os.path.isdir(install_dir):
-            bzr.info('bzr repository: %s already exists, updating.' % install_dir)
-            branch = Branch.open(install_dir)
-            branch.pull(branch.get_parent())
+            _logger.info('bzr repository: %s already exists, updating.' % install_dir)
+            bzr.bzr_pull(install_dir)
         else:
             #TODO: add the option for the user to install another version of oerptools
             #the prefered method of selection is with a command line option.
             
-            #Make config directory
-            if not os.path.exists('/etc/oerptools'):
-                os.mkdir('/etc/oerptools')
-            
-            #Get the branch
-            # Load the bzr plugins - the "launchpad" plugin provides support for the "lp:" shortcut
-            load_plugins()
-            remote_branch = Branch.open('lp:oerptools')
-            branch = remote_branch.bzrdir.sprout(install_dir).open_branch()
+            # Copy this branch
+            branch = bzr.bzr_branch(branch_path, install_dir)
+            # Update from lp
+            branch.set_parent('lp:oerptools')
+            bzr.bzr_pull(install_dir)
+        
+        #Make config directory
+        if not os.path.exists('/etc/oerptools'):
+            os.mkdir('/etc/oerptools')
         
         # Update config
-        if os.path.isfile(install_dir):
-            
+        if not os.path.exists(values['main']['config_file']):
+            shutil.copy(branch_path+"/install/default-oerptools.conf",values['main']['config_file'])
+        elif not os.path.isfile(values['main']['config_file']):
+            _logger.warning('Config file (%s) not updated because it is not a regular file.' % values['main']['config_file'])
         
-        tools.exec_command('')
-
-        return
+        config.params.update_config_file_values(values, update_file=values['main']['config_file'])
         
-    '''
-            cat > /etc/openerp-ccorp-scripts/settings.cfg <<EOF
-    repo="lp:openerp-ccorp-scripts"
-    branch=$branch
-    EOF
-        fi
-        cd $dir
-        . ccorp-openerp-scripts-update.sh
-        echo "If this is the first time you are running this script, please run 'export OPENERP_CCORP_DIR=/usr/local/share/openerp-ccorp-scripts'"
-    fi
-    echo ""
-    '''
+        # Make bin symlink to oerptools.py
+        if os.path.islink('/usr/local/bin/oerptools'):
+            os.remove('/usr/local/bin/oerptools')
+            os.symlink(install_dir+'/oerptools.py','/usr/local/bin/oerptools')
+        elif os.path.exists('/usr/local/bin/oerptools'):
+            _logger.warning('/usr/local/bin/oerptools already exists and is not a symlink.')
+        else:
+            os.symlink(install_dir+'/oerptools.py','/usr/local/bin/oerptools')
+        
+        # Create log dir
+        log_file = os.path.abspath(values['logging']['log_file'])
+        log_dir = os.path.dirname(log_file)
+        if not os.path.isdir(log_dir) and os.path.exists(log_dir):
+            _logger.warning('The log directory (%s) already exists but isn\'t a valid directory.' % log_dir)
+        elif not os.path.isdir(log_dir):
+            _logger.debug('Making the log directory: %s' % log_dir)
+            os.makedirs(log_dir)
 
+        return True
+    
     def arch_oerptools_install(install_dir):
         #TODO: logger gets the output of the command
         print('Installing bzr...')
         print('')
         tools.exec_command('pacman -Sy --noconfirm bzr')
         return
+
+
+    _logger.debug('Checking if user is root')
+    tools.exit_if_not_root('oerptools-install')
+    
+    _logger.debug('Installing bzr')
+    bzr.bzr_install()
+        
+    if 'install_target_path' in config.params:
+        install_dir = config.params['install_target_path']
+    else:
+        install_dir = '/usr/local/share/oerptools'
+    
+    oerptools_install(install_dir)
+    
+    return True
