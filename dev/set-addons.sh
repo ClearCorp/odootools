dd1c46d4887a70e5ce65680cf06ceda673b3f8ca
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2016-01-28 16:10:26 -0600

    [FIX] set-addons.sh

--- dev/set-addons.sh
+++ dev/set-addons.sh
@@ -4,15 +4,15 @@ CONF_FILE=$1
 VERSION=$2
 
 # Backup config file
-cp -a $CONF_FILE .$CONF_FILE.bak
+cp -a $CONF_FILE $CONF_FILE.bak
 
 
 # Change addons in file
-odoo_list=$(for repo in `ls -1d /opt/odoo/'$VERSION'/odoo/*`; do echo -n "		"; echo $repo","; done; echo "\n")
-clearcorp_list=$(for repo in `ls -1d /opt/odoo/'$VERSION'/clearcorp/*`; do echo -n "		"; echo $repo","; done; echo "\n")
-custom_list=$(for repo in `ls -1d /opt/odoo/'$VERSION'/custom/*`; do echo -n "		"; echo $repo","; done; echo "\n")
-oca_list=$(for repo in `ls -1d /opt/odoo/'$VERSION'/oca/*`; do echo -n "		"; echo $repo","; done; echo "\n")
-other_list=$(for repo in `ls -1d /opt/odoo/'$VERSION'/other/*`; do echo -n "		"; echo $repo","; done; echo "\n")
+odoo_list=$(for repo in `ls -1d /opt/odoo/$VERSION/odoo/*`; do echo -n "		"; echo $repo","; done; echo "\n")
+clearcorp_list=$(for repo in `ls -1d /opt/odoo/$VERSION/clearcorp/*`; do echo -n "		"; echo $repo","; done; echo "\n")
+custom_list=$(for repo in `ls -1d /opt/odoo/$VERSION/custom/*`; do echo -n "		"; echo $repo","; done; echo "\n")
+oca_list=$(for repo in `ls -1d /opt/odoo/$VERSION/oca/*`; do echo -n "		"; echo $repo","; done; echo "\n")
+other_list=$(for repo in `ls -1d /opt/odoo/$VERSION/other/*`; do echo -n "		"; echo $repo","; done; echo "\n")
 
 addons="# Addons start - DO NOT REMOVE THIS LINE\n\
 $odoo_list\
